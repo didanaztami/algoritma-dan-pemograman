028159ef80d7788484ff5a4002b209426453317a
didanaztami <didanaztami2802@gmail.com>
2025-04-30 19:12:26 +0700

    project halo pert 9
    project halo

---
+++ dida9halo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+	string kata;
+	cout<<"Masukkan kata = HALO"<<endl;
+	cin>>kata;
+	
+	if (kata=="HALO"){
+		cout<<"kata yang dimasukkan sesuai"<<endl;
+	}else{
+		cout<<"kata yang dimasukkan tidak sesuai"<<endl;
+	}
+}
+
